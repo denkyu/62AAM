d27650f7a6d9dbb9f01947a55ba0389e6ec55e9d
denkyu <denkyu36@gmail.com>
2017-10-02 20:24:29 +0900

    Project's Initial Commit

---
+++ www/js/web.js
@@ -0,0 +1,27 @@
+// This is a JavaScript file
+function site62(){
+var ref = window.open('http://azbjichi.xsrv.jp/', '_self', 'location=no,presentationstyle=fullscreen,toolbar=yes');
+}
+function bbc(){
+var ref = window.open('http://azb-thr.com/', '_self', 'location=no,presentationstyle=fullscreen,toolbar=yes');
+}
+function activity(){
+var ref = window.open('http://azbjichi.xsrv.jp/category/%E6%B4%BB%E5%8B%95%E5%A0%B1%E5%91%8A/', '_self', 'location=no,presentationstyle=fullscreen,toolbar=yes');
+}
+function score(){
+var ref = window.open('http://azbjichi.xsrv.jp/score.html', '_self', 'location=no,presentationstyle=fullscreen,toolbar=yes');
+}
+function mail(){
+var mailto = "mailto:62soukatu@gmail.com";
+mailto = mailto +"?subject=タイトル&body=本文";
+location.href= mailto;
+}
+function twitter(){
+var ref = window.open('https://twitter.com/62aam?lang=ja', '_self', 'location=no,presentationstyle=fullscreen,toolbar=yes');
+}
+function jikkyo(){
+var ref = window.open('http://azbjichi.xsrv.jp/jikkyo.html', '_self', 'location=no,presentationstyle=fullscreen,toolbar=yes');
+}
+function yosou(){
+var ref = window.open('http://azbjichi.xsrv.jp/yosou.html', '_self', 'location=no,presentationstyle=fullscreen,toolbar=yes');
+}
